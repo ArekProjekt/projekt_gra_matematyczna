854164cd6931a1207ce106a504625d92a4073de1
ArekProjekt <150654916+ArekProjekt@users.noreply.github.com>
2023-11-13 08:40:42 +0100

    Update Passwd.js

--- Passwd.js
+++ Passwd.js
@@ -2,7 +2,7 @@ function checkPassword() {
     var password = document.getElementById("password").value;
  
     // Replace "your_password" with the actual password you want to use
-    if (password === "your_password") {
+    if (password === "L1ZFLSTROEN") {
         document.getElementById("gallery").style.display = "block";
     } else {
         alert("Incorrect password. Try again.");
